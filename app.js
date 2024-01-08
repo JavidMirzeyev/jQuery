ec63121fd1a6ddb958d387d39f03f8943e29ceb0
Javid Mirzeyev <javid.mirzeyev+demo@gmail.com>
2024-01-08 20:25:18 +0000

    webflow

--- app.js
+++ app.js
@@ -2,26 +2,19 @@
 $(document).ready(function () {
 
 
-
-
-
-
-
     $('.btn').click(function (e) {
         e.preventDefault();
         if (!$.trim($('input').val()) == '') {
             let a = b => {
-                for (let i = 0; i < b; i++) { $('.list').append(`<h3>${b}</h3>`); }
-            };
-            let v = f => Number(f) ? a(f) : $('.list').append(`<h3>${f}</h3>`);
+                for (let i = 0; i < b; i++) { $('.minibox').append(`<h2>${b}</h2>`); }
+            }
+            let v = f => Number(f) ? a(f) : $('.minibox').append(`<h2>${f}</h2>`);
             v($('input').val());
 
         }
         $('input').val('');
 
     })
-
-
 
 
 
